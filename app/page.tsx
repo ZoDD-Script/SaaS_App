4e45aa9d74a0b0bcbe11d886d55ec9fa8877e9ad
ZoDD-Script <blessedchukwu89@gmail.com>
2025-07-21 17:15:15 +0100

    Implement companio card ui on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,45 @@
-import React from 'react'
-import {Button} from "@/components/ui/button";
+import React from "react";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-4xl text-red-500 underline hover:text-xs">Welcome to my SaaS App.</h1>
-      <Button>Lets get Started.</Button>
-    </div>
-  )
-}
+    <main>
+      <h1 className="">Popular Companions.</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="Science"
+          duration={45}
+          color="#E5D0FF"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="Maths"
+          duration={55}
+          color="#FFDA6E"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="Language"
+          duration={29}
+          color="#BDE7FF"
+        />
+      </section>
 
-export default Page
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
